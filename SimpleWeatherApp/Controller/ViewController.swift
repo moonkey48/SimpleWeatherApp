f2bed55add543e9cc66a161b247dff27a80ea401
= <moonkey48@icloud.com>
2023-02-28 21:05:46 +0900

    initial commit

---
+++ SimpleWeatherApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleWeatherApp
+//
+//  Created by moonkey on 2023/02/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SimpleWeatherApp/Model/WeatherManager.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherManager.swift
+//  SimpleWeatherApp
+//
+//  Created by moonkey on 2023/02/28.
+//
+
+import Foundation
+
+final class WeatherManager {
+    
+    
+    let shared = WeatherManager()
+    
+    private init() {}
+}
