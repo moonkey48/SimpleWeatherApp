4dec300e2c13fc590c5e1604eda3e8fcfc1df248
= <moonkey48@icloud.com>
2023-02-28 22:29:21 +0900

    [feat] add WeatherNetwork Manager

--- SimpleWeatherApp/Controller/ViewController.swift
+++ SimpleWeatherApp/Controller/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let weatherManager = WeatherManager.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupData()
+    }
+    func setupData(){
+        weatherManager.getWeatherInfo { result in
+            switch result {
+            case .success(let wetherData):
+                print(wetherData.weather[0].main)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 
 
---
+++ SimpleWeatherApp/Helpers/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  SimpleWeatherApp
+//
+//  Created by moonkey on 2023/02/28.
+//
+
+import Foundation
+
+public struct WeatherAPI {
+    static let requestURL = "https://api.openweathermap.org/data/2.5/weather?"
+    static let params = "lat=37.5&lon=127"
+    private init() {}
+}
